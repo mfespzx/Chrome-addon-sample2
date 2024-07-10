c8e45870ea27ee70ff12dc6a894493ca675ab164
a24598 <atshsmt@gmail.com>
2024-07-10 23:48:52 +0900

    Add files via upload
    mod

--- content_script.js
+++ content_script.js
@@ -1,109 +1,136 @@
 "use strict";
-window.addEventListener("load", (event) => {
+window.addEventListener("load", function (event) {
     Notification.requestPermission();
-    let body = document.getElementById('Content');
-    if(body){
-        body.style.background = '#cccccc';
-        if(location.href.includes("search")){
-            window.scrollTo({ 
-                top: 500, 
-                behavior: 'smooth' 
+    var body = document.getElementById('Content');
+    if (body && location.href.includes("crowdworks.jp")) { // 一覧画面(CW)
+        body.style.background = '#aaaaaa';
+        if (location.href.includes("search")) {
+            window.scrollTo({
+                top: 500,
+                behavior: 'smooth'
             });
+            setTimeout(cw_job, 2000);
         }
-        const joblist = document.getElementsByClassName('UMNkI');
-            if(Object.prototype.toString.call(joblist) === "[object HTMLCollection]"){
-                for(var i = 0; i < 10; i++){
-                    const str = joblist[i].textContent;
-                    if(str.match(/検索/) || str.match(/コピペ/)){
-                        var cookie = document.cookie;
-                        if(cookie.match(/crw_sech\=(.*?)\;/)){
-                            const match = cookie.match(/crw_sech\=(.*?)\;/);
-                            cookie = match[1];
-                        } else {
-                            cookie = null;
+    }
+    else if (location.href.includes("lancers.jp/work/search")) { // 一覧画面(LS)
+        var body2 = document.getElementsByClassName('l-wrapper');
+        var back = body2[0].getElementsByClassName('p-search-job');
+        var back2 = body2[0].getElementsByClassName('p-search-job__right');
+        back[0].style.background = '#999999';
+        back2[0].style.background = '#999999';
+        if (location.href.includes("search?")) {
+            var joblist = document.getElementsByClassName('p-search-job-media');
+            if (Object.prototype.toString.call(joblist) === "[object HTMLCollection]") {
+                for (var i = 0; i < 20; i++) {
+                    if (joblist[i] !== null) {
+                        var str = joblist[i].getElementsByClassName('p-search-job-media__title');
+                        var delstr = str[0].getElementsByClassName("p-search-job-media__tags");
+                        str[0].removeChild(delstr[0]);
+                        var str_af = str[0].textContent;
+                        if (str_af) {
+                            str_af.replace(/\s+/g, '');
+                            if (str_af.match(/スマホで/) || str_af.match(/検索/) || str_af.match(/コピペ/) || str_af.match(/学術/) || str_af.match(/心理学/) || str_af.match(/ヘッドホン/)) {
+                                var workid = str[0].href;
+                                var match2 = workid.match(/(.*?)\/(\d{7,})/);
+                                if (match2) {
+                                    workid = match2[2];
+                                    var lan_url = sessionStorage.getItem('lan_url');
+                                    var lan_sech = sessionStorage.getItem('lan_sech');
+                                    console.log("url:" + lan_url);
+                                    console.log("st:" + str_af);
+                                    if (lan_url !== workid) {
+                                        sessionStorage.setItem('lan_sech', str_af);
+                                        sessionStorage.setItem('lan_url', workid);
+                                        console.log("id:" + workid);
+                                        new Notification("Lan Check!" + str_af);
+                                    }
+                                    break;
+                                }
+                            }
                         }
-                        if(cookie != str){
-                           document.cookie = "crw_sech=" + str;
-                            console.log(workid);
-                            const notification = new Notification("Crw Check!");
-                        }
-                        break;
                     }
                 }
-            } else {
-const notification2 = new Notification(joblist);
             }
-    } else {
-        if(location.href.includes("search?")){
-            const joblist = document.getElementsByClassName('p-search-job-media');
-            if(Object.prototype.toString.call(joblist) === "[object HTMLCollection]"){
-                for(var i = 0; i < 10; i++){
-                    const str = joblist[i].getElementsByClassName('p-search-job-media__title');
-                    let delstr = str[0].getElementsByClassName("p-search-job-media__tags");
-                    str[0].removeChild(delstr[0]);
-                    const str_af = str[0].textContent;
-                    if(str_af.match(/検索/) || str_af.match(/コピペ/) || str_af.match(/学術/)){
-                        var cookie = document.cookie;
-                        if(cookie.match(/lan_sech\=(\d{7,})\;/)){
-                            const match = cookie.match(/lan_sech\=(\d{7,})\;/);
-                            cookie = match[1];
-                        } else {
-                            cookie = null;
-                        }
-                        var workid = str[0].href;
-                        const match2 = workid.match(/(.*?)\/(\d{7,})/);
-                        workid = match2[2];
-                        if(cookie != workid){
-                            document.cookie = "lan_sech=" + workid;
-                            console.log(workid);
-                            const notification = new Notification("Lan Check!");
-                        }
-                        break;
-                    }
-                }
-            } else {
-const notification2 = new Notification(joblist);
+            else {
+                new Notification(joblist.toString());
             }
         }
     }
+    function cw_job() {
+        var joblist = document.getElementsByClassName('UMNkI');
+        if (Object.prototype.toString.call(joblist) === "[object HTMLCollection]") {
+            for (var i = 0; i < 20; i++) {
+                if (joblist[i] !== null) {
+                    var str = joblist[i].textContent;
+                    var strUrl = joblist[i].href;
+                    if (str) {
+                        if (str.match(/検索/) || str.match(/コピペ/) || str.match(/心理学/) || str.match(/学術/) || str.match(/ヘッドホン/)) {
+                            var crw_url = sessionStorage.getItem('crw_url');
+                            var crw_sech = sessionStorage.getItem('crw_sech');
+                            console.log("st:" + str);
+                            console.log("url:" + crw_url);
+                            if (strUrl !== crw_url) {
+                                sessionStorage.setItem('crw_sech', str);
+                                sessionStorage.setItem('crw_url', strUrl);
+                                new Notification("Crw Check!" + str);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            new Notification(joblist.toString());
+        }
+    }
+    if (location.href.includes("lancers.jp/") || location.href.includes("crowdworks.jp/") || location.href.includes("lockkanre.com/")) {
+        document.body.addEventListener("selectstart", function () {
+            var fn;
+            var e;
+            var se;
+            var _a, _b;
+            document.body.addEventListener("keyup", fn = function (e) {
+                e.stopImmediatePropagation();
+                document.body.removeEventListener("keyup", fn);
+                if (e.keyCode === 0x10) {
+                    var selectedText = ((_b = window.getSelection()) === null || _b === void 0 ? void 0 : _b.toString()) || '';
+                    console.log(selectedText);
+                    window.open("http://127.0.0.1/get/getword.php?text=".concat(selectedText), 'getword');
+                }
+            });
+            /*
+            document.body.addEventListener("keyup", fn = (e) => {
+                event.stopImmediatePropagation();
+                document.body.removeEventListener("keyup", fn);
+                if(e.keyCode === 96 || e.keyCode === 97){
+                    searchNum(e, se = 0);
+                } else if (e.keyCode === 71){ // search Google
+                    document.body.addEventListener("keyup", fn = (e) => {
+                        event.stopImmediatePropagation();
+                        document.body.removeEventListener("keyup", fn);
+                        if(e.keyCode === 96 || e.keyCode === 97){
+                            searchNum(e, se = "g");
+                        }
+                    });
+                }
+            });
 
-
-   document.body.addEventListener("selectstart", () => {
-        let fn;
-        let e;
-        let se;
-        var _a, _b;
-        document.body.addEventListener("keyup", fn = (e) => {
-            event.stopImmediatePropagation();
-            document.body.removeEventListener("keyup", fn);
-            if(e.keyCode === 96 || e.keyCode === 97){
-                searchNum(e, se = 0);
-            } else if (e.keyCode === 71){ // search Google
-                document.body.addEventListener("keyup", fn = (e) => {
+            function searchNum(e, se){
+                let fn2;
+                document.body.addEventListener("keyup", fn2 = (e2) => {
                     event.stopImmediatePropagation();
-                    document.body.removeEventListener("keyup", fn);
-                    if(e.keyCode === 96 || e.keyCode === 97){
-                        searchNum(e, se = "g");
+                    document.body.removeEventListener("keyup", fn2);
+                    if ((e2.keyCode >= 96 && e2.keyCode <= 105) && ((_a = window.getSelection()) === null || _a === void 0 ? void 0 : _a.toString())) {
+                        const selectedText = ((_b = window.getSelection()) === null || _b === void 0 ? void 0 : _b.toString()) || '';
+                        console.log(selectedText);
+                        window.open(`http://127.0.0.1/get/getword.php?text=${selectedText}&no=${e.keyCode - 96}${e2.keyCode - 96}&se=${se}`, 'getword');
+                    } else {
+                        console.log('Fail');
                     }
                 });
             }
+            */
         });
-
-        function searchNum(e, se){
-            let fn2;
-            document.body.addEventListener("keyup", fn2 = (e2) => {
-                event.stopImmediatePropagation();
-                document.body.removeEventListener("keyup", fn2);
-                if ((e2.keyCode >= 96 && e2.keyCode <= 105) && ((_a = window.getSelection()) === null || _a === void 0 ? void 0 : _a.toString())) {
-                    const selectedText = ((_b = window.getSelection()) === null || _b === void 0 ? void 0 : _b.toString()) || '';
-                    console.log(selectedText);
-                    window.open(`http://127.0.0.1/get/getword.php?text=${selectedText}&no=${e.keyCode - 96}${e2.keyCode - 96}&se=${se}`, 'getword');
-                } else {
-                    console.log('Fail');
-                }
-            });
-        }
-
-    });
+    }
 });
-
